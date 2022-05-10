1f3a1c1f4dfbfd2b1316a67613f87a7e444e40ef
Tien <nguyenquoctien2000@gmail.com>
2022-05-10 17:36:14 +0700

    Hoàn thành Razzi - thiếu header

--- css/app.js
+++ css/app.js
@@ -69,28 +69,77 @@ $(document).ready(function () {
   });
 });
 // STYLE BODY-BRANCH
-// $(document).ready(function () {
-//   $(".branch-item").slick({
-//     slidesToShow: 7,
-//     dots: false,
-//     arrows: false,
-//     responsive: [
-//       {
-//         breakpoint: 1025,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 4,
-//           dots: true,
-//         },
-//       },
-//       {
-//         breakpoint: 668,
-//         settings: {
-//           slidesToShow: 2,
-//           dots: true,
-//           slidesToScroll: 2,
-//         },
-//       },
-//     ],
-//   });
-// });
+$(document).ready(function () {
+  $(".branch-item").slick({
+    slidesToShow: 7,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 668,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+});
+// STYLE MODAL SELLERS CART
+$(document).ready(function () {
+  $(".modal-footer-cart-slick").slick({
+    infinite: false,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          arrows: false,
+          dots: true,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 668,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          dots: true,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
+
+// STYLE MODAL SELLERS VIEW
+$(document).ready(function () {
+  $(".body-view-img-slick").slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow:
+      "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+      "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
