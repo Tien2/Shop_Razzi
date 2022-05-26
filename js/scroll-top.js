fd37cce0f4fa5abaee20aab60857ab1b9b6ca990
Tien <nguyenquoctien2000@gmail.com>
2022-05-26 17:23:45 +0700

    update new

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,8 +1,18 @@
+// SCROLL TO TOP
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 300) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
+  }
+});
+// SCROLL CART TO TOP
+const stickyTop = document.querySelector(".stickyTop");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 500) {
+    toTop.classList.add("sticky-top");
+  } else {
+    toTop.classList.remove("sticky-top");
   }
 });
