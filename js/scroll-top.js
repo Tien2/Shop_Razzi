dc4d700feba3a94aad7b30cdce9a422a0eaf7002
Tien <nguyenquoctien2000@gmail.com>
2022-05-27 16:47:20 +0700

    update sticky item on top t-shirts

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,19 +1,30 @@
 // SCROLL TO TOP
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 300) {
+  if (window.pageYOffset > 500) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
   }
 });
-// SCROLL CART TO TOP
-// const stickyTop = document.querySelector(".stickyTop");
-// window.addEventListener("scroll", () => {
-//   if (window.pageYOffset > 500) {
-//     toTop.classList.add("sticky-top");
-//   } else {
-//     toTop.classList.remove("sticky-top");
-//   }
-// });
-``;
+// STICKY NAVBAR
+var elementPosition = $("#Top_bar").offset();
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > elementPosition.top) {
+    $("#Top_bar").addClass("Top_bar-fixed");
+    $("#Top_bar").removeClass("Top_bar");
+  } else {
+    $("#Top_bar").removeClass("Top_bar-fixed");
+    $("#Top_bar").addClass("Top_bar");
+  }
+});
+// STICKY ITEM CART ON TOP
+const Top = document.querySelector(".to-top1");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 900) {
+    Top.classList.add("active1");
+  } else {
+    Top.classList.remove("active1");
+  }
+});
